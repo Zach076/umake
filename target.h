dc29b089b313958deeae4ceda8279c1cff5b99fa
richarz <richarz@wwu.edu>
2018-02-02 16:14:00 -0800

    typedef for custom function stuff

--- target.h
+++ target.h
@@ -3,12 +3,13 @@
 
 struct target_st;
 typedef struct target_st target;
+typedef void(*list_action)(char*);
 
 target* new_target(char* name);
 target* find_target(char* name);
 void add_dependency_target(target* tgt, char* dep);
 void add_rule_target(target* tgt, char* rule);
-void for_each_rule(target* tgt, void(*action)(char*));
-void for_each_dependency(target* tgt, void(*action)(char*));
+void for_each_rule(target* tgt, list_action action);
+void for_each_dependency(target* tgt, list_action action);
 
 #endif
